57fc327d1891a517489edd5e6aeab7140c8f40a4
AntoineLRK <admin@antoinelrk.com>
2023-02-06 07:51:38 +0100

    Added informations in tube show page

--- resources/views/tubes/showTube.blade.php
+++ resources/views/tubes/showTube.blade.php
@@ -12,6 +12,20 @@
         <div class="tube-data">
             <ul>
                 <li>
+                    <span>Added by</span>&nbsp;
+                    {{ $tube->user->name }}
+                </li>
+                <li>
+                    <span>Created at</span>&nbsp;
+                    {{ $tube->created_at->format('d/m/Y') }}
+                </li>
+                <li>
+                    <span>Updated at</span>&nbsp;
+                    {{ $tube->updated_at->format('d/m/Y') }}
+                </li>
+            </ul>
+            <ul>
+                <li>
                     <span>Quantity</span>&nbsp;
                     <i class="@if ($tube->quantity <= $tube->critical) critical @elseif ($tube->quantity <= $tube->warning) warning @endif">
                         {{ $tube->quantity }}
@@ -31,7 +45,7 @@
 
     <div class="tube-control">
         <a class="btn edit" href="{{ route('tubes.updateTubeForm', $tube->slug) }}">Editer</a>
-        <a class="btn delete" href="{{ route('tubes.deleteTubeForm', $tube->slug) }}">
+        <a class="btn delete js-delete-tube" data-tube="{{ $tube }}">
             <figure>
                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 448 512">
                     <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
