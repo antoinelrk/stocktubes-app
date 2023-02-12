217415b21e198ae5b63d7fad4300f5958ae117f6
Antoine LRK <admin@antoinelrk.com>
2023-02-12 15:46:58 +0100

    Prepare mega-save with zip

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PagesController::class, 'home'])
     ->name('home');
+Route::get('/test', [PagesController::class, 'zipDatasheets'])
+    ->name('test');
 
 Route::group(['prefix' => 'auth'], function () {
     Route::get('/login', [LoginController::class, 'showLoginForm'])
